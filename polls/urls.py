43462fe6bd57706bef8bace8cabb9743ed3c111c
chriskang <chriskang92@gmail.com>
2025-06-02 15:54:10 +0900

    (feat) _ header & footer & HTML with CSS

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"  # 이 앱의 URL 네임스페이스를 설정합니다. 다른 앱과 URL이 겹치지 않도록 합니다.
+
 urlpatterns = [
     path("", views.index, name="index"), # mysite 에서 polls 앱의 index 뷰를 호출 했기 때문에  http://127.0.0.1:8000/polls/ 로 접근하면 index 뷰가 호출됨
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,40 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
+from django.shortcuts import get_object_or_404
 
+# index(최신글 list)
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    html = """
-<html>
-<head>
-        <title>투표 사이트</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f2f2f2;
-                padding: 2em;
-            }
-            h1 {color: #333399;}
-            .box {
-                background: white;
-                padding: 1.5em;
-                border-radius: 10px;
-                box-shadow: 0 0 10px rgba(0,0,0,0.1);
-                max-width: 600px;
-                margin: auto;
-            }
-        </style>
-</head>
+    # return HttpResponse("Hello, world. You're at the polls index."
 
-<body>
-        <div class="box">
-            <h1>안녕하세요!</h1>
-            <p>여기는 <strong>설문조사(polls)</strong> 
-            사이트의 메인 페이지입니다.
-            </p>
-            <p>관리자는 설문을 추가하고, 사용자는 투표할 수 있습니다.</p>
-        </div>
-</body>
+    latest_question_list = Question.objects.order_by("-pub_date")[:5] #- pub_date는 최신순으로 정렬
+    # latest_question_list = Question.objects.all() # 전체 조회 
+    # latest_question_list = Question.objects.filter(question_text__contains="Python") # question_text에 "Python"이 포함된 질문 조회
+    # latest_question_list = Question.objects.filter(pub_date__year=2023) # 2023년에 게시된 질문 조회
+    # latest_question_list = Question.objects.filter(pub_date__year=2023, pub_date__month=10) # 2023년 10월에 게시된 질문 조회
+    # latest_question_list = Question.objects.filter(pub_date__year=2023, pub_date__month=10, pub_date__day=1) # 2023년 10월 1일에 게시된 질문 조회
+    # latest_question_list = Question.objects.exclude(question_text__contains="Python") # question_text에 "Python"이 포함되지 않은 질문 조회
+    context = {"latest_question_list": latest_question_list}#index.html에서 사용할 변수
+    return render(request, "polls/index.html", context)
 
-</html>
-"""
-    return HttpResponse(html) # HttpResponse 객체를 반환하여 HTML 콘텐츠를 응답으로 보냄
+# detail(상세조회)
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
