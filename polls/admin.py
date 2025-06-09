1656acea2d04c26c1014e13b04137eebed24f402
chriskang <chriskang92@gmail.com>
2025-06-09 17:59:37 +0900

    feat_part 6

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+# 위는 기존 코드 (등록의 역할)
+
+from django.contrib import admin
+from .models import Question, Choice
+
+# Choice 인라인 (Tabular 형식)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# Question 모델 커스터마이징
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("제목", {"fields": ["question_text"], "classes" : ["collapse"]}),
+        ("날짜정보", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+# 등록
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
